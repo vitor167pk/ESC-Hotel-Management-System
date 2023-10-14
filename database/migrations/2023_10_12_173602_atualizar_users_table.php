0afaf5794205f924a9ee0fd756a64f6192dfde78
EVRSON AVIZ <99806962+eversonaviz70@users.noreply.github.com>
2023-10-13 22:06:27 -0300

    update laravel jetstream

---
+++ database/migrations/2023_10_12_173602_atualizar_users_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->integer('status'); // Remova o valor padrão
+           // $table->text('two_factor_secret')->nullable();
+            //$table->timestamp('two_factor_confirmed_at')->nullable();
+            $table->string('sub', 255)->nullable();
+            $table->integer('profile')->nullable();
+            $table->integer('edited_by')->nullable();
+            $table->string('picture', 255)->nullable();
+            $table->integer('branch')->nullable();
+            $table->softDeletes();
+            $table->uuid('updated_by')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
